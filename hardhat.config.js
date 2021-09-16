db78b0ef2a47841e8eadc00e03d6317653893d34
reunion-island-art-digitizer <88419764+reunion-island-art-digitizer@users.noreply.github.com>
2021-09-16 16:53:11 +0400

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,34 @@
+/* hardhat.config.js */
+require("@nomiclabs/hardhat-waffle")
+const fs = require('fs')
+const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789"
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+     },
+    mumbai: {
+      // Infura
+      // url: `https://polygon-mumbai.infura.io/v3/${infuraId}`
+      url: "https://rpc-mumbai.matic.today",
+      accounts: [df57089febbacf7ba0bc227dafbffa9fc08a93fdc68e1e42411a14efcf23656e]
+    },
+    matic: {
+      // Infura
+      // url: `https://polygon-mainnet.infura.io/v3/${infuraId}`,
+      url: "https://rpc-mainnet.matic.network",
+      accounts: [df57089febbacf7ba0bc227dafbffa9fc08a93fdc68e1e42411a14efcf23656e]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+}
